ec8332e90a32dd0c1384b478c9ade1b26adbfe16
Peshal Agarwal <peshala48@gmail.com>
2020-10-01 11:23:37 +0200

    Reformat

--- CIFAR10/Full/tune.py
+++ CIFAR10/Full/tune.py
@@ -42,21 +42,21 @@ def train(net, perturb, batch_size, optimizer, testloader, device, epochs):
 def main():
     torch.manual_seed(1)
     parser = argparse.ArgumentParser(description="PyTorch CIFAR10 Training")
-    parser.add_argument("--batch_size", type=int, default=128, help="Test batch size")
-    parser.add_argument(
-        "--perturb", type=float, default=0.0, help="Magnitude of noise to the input"
-    )
+    parser.add_argument("--batch_size", type=int, default=128,
+                        help="Test batch size")
+    parser.add_argument("--perturb", type=float, default=0.0,
+                        help="Magnitude of noise to the input")
     parser.add_argument("--lr", default=1e-5, type=float, help="learning rate")
-    parser.add_argument(
-        "--epochs", type=int, default=10, help="Number of epochs to fine tune"
-    )
+    parser.add_argument("--epochs", type=int, default=10,
+                        help="Number of epochs to fine tune")
     args = parser.parse_args()
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     print("==> Preparing data..")
 
-    trainloader, testloader = read_vision_dataset("./data", batch_size=args.batch_size)
+    trainloader, testloader = read_vision_dataset("./data",
+                                                  batch_size=args.batch_size)
 
     print("==> Building model..")
 
@@ -68,10 +68,10 @@ def main():
     checkpoint = torch.load("ckpt.pth", map_location=device)
     net.load_state_dict(checkpoint["net"])
     net.eval()
-    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-    train(
-        net, args.perturb, args.batch_size, optimizer, testloader, device, args.epochs
-    )
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9,
+                          weight_decay=5e-4)
+    train(net, args.perturb, args.batch_size, optimizer,
+          testloader, device, args.epochs)
 
 
 if __name__ == "__main__":
