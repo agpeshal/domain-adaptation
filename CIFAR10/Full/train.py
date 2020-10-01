5df332bb0fcdd1789697dc70fcabbc89596f0942
Peshal Agarwal <peshala48@gmail.com>
2020-10-01 10:45:49 +0200

    Make modular
    Removed unnecessary function from utils and made train.py modular
    Not tested yet. Potential error could be due to "best_acc" var in
    test function of train.py

--- CIFAR10/Full/train.py
+++ CIFAR10/Full/train.py
@@ -1,109 +1,18 @@
-'''Train CIFAR10 with PyTorch.'''
+"""Train CIFAR10 with PyTorch."""
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
 
-import torchvision
-import torchvision.transforms as transforms
-
-import os
 import argparse
 
-from models import *
-# from utils import progress_bar
+from resnet import ResNet18
+from utils import read_vision_dataset
 from tqdm import tqdm
-from select_classes import get_class_i, DatasetMaker
 
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', action='store_true',
-                    help='resume from checkpoint')
-args = parser.parse_args()
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=None)
-
-
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=None)
-# testloader = torch.utils.data.DataLoader(
-#     testset, batch_size=100, shuffle=False, num_workers=2)
-
-classDict = {'plane':0, 'car':1, 'bird':2, 'cat':3, 'deer':4, 'dog':5, 'frog':6, 'horse':7, 'ship':8, 'truck':9}
-
-# Separating trainset/testset data/label
-x_train  = trainset.data
-x_test   = testset.data
-y_train  = trainset.targets
-y_test   = testset.targets
-
-# Let's choose cats (class 3 of CIFAR) and dogs (class 5 of CIFAR) as trainset/testset
-car_truck_trainset = \
-    DatasetMaker(
-        [get_class_i(x_train, y_train, classDict['truck']), get_class_i(x_train, y_train, classDict['car'])],
-        transform_train
-    )
-car_truck_testset  = \
-    DatasetMaker(
-        [get_class_i(x_test , y_test , classDict['truck']), get_class_i(x_test , y_test , classDict['car'])],
-        transform_test
-    )
-trainloader = torch.utils.data.DataLoader(
-    car_truck_trainset, batch_size=128, shuffle=True, num_workers=2)
-testloader = torch.utils.data.DataLoader(
-    car_truck_testset, batch_size=512, shuffle=True, num_workers=2)
-# Model
-print('==> Building model..')
-# net = VGG('VGG19')
-
-
-net = ResNet18(num_classes=2)
-# net = nn.Sequential(Normalize(), net)
-print(net)
-net = net.to(device)
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
-
-if args.resume:
-    # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
-    net.load_state_dict(checkpoint['net'])
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                      momentum=0.9, weight_decay=5e-4)
-
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=40, gamma=0.1)
 # Training
-def train(epoch):
-    print('\nEpoch: %d' % epoch)
+def train(net, criterion, optimizer, trainloader, device, epoch):
+    print("\nEpoch: %d" % epoch)
     net.eval()
     train_loss = 0
     correct = 0
@@ -121,14 +30,14 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-    print('Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                 % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-    # progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-        #              % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    print(
+        "Loss: %.3f | Acc: %.3f%% (%d/%d)"
+        % (train_loss / (batch_idx + 1), 100.0 * correct / total, correct, total)
+    )
 
 
-def test(epoch):
-    global best_acc
+def test(net, criterion, testloader, best_acc, device, epoch):
+
     net.eval()
     test_loss = 0
     correct = 0
@@ -144,27 +53,54 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-        print('Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
-        # progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            #              % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        print(
+            "Loss: %.3f | Acc: %.3f%% (%d/%d)"
+            % (test_loss / (batch_idx + 1), 100.0 * correct / total, correct, total)
+        )
 
     # Save checkpoint.
-    acc = 100.*correct/total
+    acc = 100.0 * correct / total
     if acc > best_acc:
-        print('Saving..')
+        print("Saving..")
         state = {
-            'net': net.state_dict(),
-            'acc': acc,
-            'epoch': epoch,
+            "net": net.state_dict(),
+            "acc": acc,
+            "epoch": epoch,
         }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.pth')
+        torch.save(state, "ckpt.pth")
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+100):
-    train(epoch)
-    test(epoch)
-    scheduler.step()
+def main():
+    parser = argparse.ArgumentParser(description="PyTorch CIFAR10 Training")
+    parser.add_argument("--lr", default=0.1, type=float, help="learning rate")
+    parser.add_argument("--batch", default=128, type=int, help="Batch Size")
+    parser.add_argument(
+        "--epochs", default=100, type=int, help="Number of training epochs"
+    )
+    args = parser.parse_args()
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    global best_acc  # best test accuracy
+    best_acc = 0
+    # Data
+    print("==> Preparing data..")
+    trainloader, testloader = read_vision_dataset("./data", batch_size=args.batch)
+    # Model
+    print("==> Building model..")
+
+    net = ResNet18(num_classes=10)
+    net = net.to(device)
+    net = torch.nn.DataParallel(net)
+
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=40, gamma=0.1)
+    criterion = nn.CrossEntropyLoss()
+    for epoch in range(1, 1 + args.epochs):
+        train(net, criterion, trainloader, optimizer, device, epoch)
+        test(net, criterion, testloader, best_acc, device, epoch)
+        scheduler.step()
+
+
+if __name__ == "__main__":
+    main()
