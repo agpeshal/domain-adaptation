c4c3219ebbd5669f6971655282ed576d8ec19b33
Peshal Agarwal <peshala48@gmail.com>
2020-10-05 13:15:41 +0200

    Adapt according to latest resnet.py, more readable and modular
    1. Adding flexibility of resnet models from resnet.py
    2. Switching to names like embedding and classifier for
    better readability of the code
    3. Setting the last layer as identity instead of changing the
    model output

--- CIFAR10/Full/domain_adaptation.py
+++ CIFAR10/Full/domain_adaptation.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 from torch.backends import cuda
 from torch.optim import Adam, lr_scheduler
 from utils import read_vision_dataset, random
-from resnet import ResNet18
+import resnet
 import argparse
 import numpy as np
 
@@ -23,12 +23,12 @@ import mlflow
 from mlflow import log_metric, log_params, log_artifacts
 
 
-class Net(nn.Module):
+class Classify(nn.Module):
     def __init__(self, domains=2, classes=10):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(512, classes)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, domains)
+        super(Classify, self).__init__()
+        self.fc1 = nn.Linear(64, classes)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, domains)
 
     def forward(self, x, alpha):
         classify = self.fc1(x)
@@ -42,8 +42,8 @@ class Net(nn.Module):
 class Adaptation:
     def __init__(
         self,
-        model,
-        splitter,
+        embedding,
+        classifier,
         criterion,
         trainloader,
         testloader,
@@ -51,17 +51,14 @@ class Adaptation:
         perturb,
         optimizer,
     ):
-        self.model = model
-        self.splitter = splitter
+        self.embedding = embedding
+        self.classifier = classifier
         self.trainloader = trainloader
         self.testloader = testloader
         self.criterion = criterion
         self.perturb = perturb
         self.device = device
         self.optimizer = optimizer
-
-    def final_layers(self, features, alpha):
-        return self.splitter(features, alpha)
 
     def calc_loss(self, classify, discriminate, labels, domains):
 
@@ -71,26 +68,27 @@ class Adaptation:
         return classification_loss, discrimination_loss
 
     def train(self, epoch, alpha):
-        self.model.train()
+        self.embedding.train()
+        self.classifier.train()
         classifier_loss = 0
         discriminator_loss = 0
         net_loss = 0
-        correct = 0
+        correct_d0 = 0
+        correct_d1 = 0
         correct_domain = 0
-        correct_noisy = 0
         total = 0
 
         for batch_idx, (images, labels) in enumerate(self.trainloader):
 
-            images = images.to(self.device)
+            images_d0 = images.to(self.device)
             labels = labels.to(self.device)
-            images_noisy = images + self.perturb * random(images.shape, self.device)
+            images_d1 = images_d0 + self.perturb * random(images.shape, self.device)
 
-            outputs = self.model(images)
-            outputs_noisy = self.model(images_noisy)
+            embeddings_d0 = self.embedding(images_d0)
+            embeddings_d1 = self.embedding(images_d1)
 
-            classify, original = self.final_layers(outputs, alpha)
-            classify_noisy, noisy = self.final_layers(outputs_noisy, alpha)
+            outputs_d0_class, outputs_d0 = self.classifier(embeddings_d0, alpha)
+            outputs_d1_class, outputs_d1 = self.classifier(embeddings_d1, alpha)
 
             # 0 for original, 1 for noisy
             domains = torch.cat(
@@ -99,12 +97,12 @@ class Adaptation:
                     torch.ones(images.size(0), device=self.device),
                 )
             ).long()
-            discriminate = torch.cat((original, noisy))
+            outputs_domains = torch.cat((outputs_d0, outputs_d1))
 
             loss_class, loss_dis = self.calc_loss(
-                classify, discriminate, labels, domains
+                outputs_d0_class, outputs_domains, labels, domains
             )
-            loss = loss_class + 0.0 * loss_dis
+            loss = loss_class + loss_dis
             loss.backward()
             self.optimizer.step()
             self.optimizer.zero_grad()
@@ -113,20 +111,20 @@ class Adaptation:
             discriminator_loss += loss_dis.item()
             net_loss += loss.item()
 
-            predicted = classify.argmax(1)
-            correct += predicted.eq(labels).sum().item()
+            predicted_d0 = outputs_d0_class.argmax(1)
+            correct_d0 += predicted_d0.eq(labels).sum().item()
 
-            predicted_noisy = classify_noisy.argmax(1)
-            correct_noisy += predicted_noisy.eq(labels).sum().item()
+            predicted_d1 = outputs_d1_class.argmax(1)
+            correct_d1 += predicted_d1.eq(labels).sum().item()
 
-            predicted_domain = discriminate.argmax(1)
+            predicted_domain = outputs_domains.argmax(1)
             correct_domain += domains.eq(predicted_domain).sum().item()
 
             total += labels.size(0)
 
         log_metric("Train/Net_loss", net_loss / (batch_idx + 1), epoch)
-        log_metric("Train/Acc_clean", 1.0 * correct / total, epoch)
-        log_metric("Train/Acc_noise", 1.0 * correct_noisy / total, epoch)
+        log_metric("Train/Acc_clean", 1.0 * correct_d0 / total, epoch)
+        log_metric("Train/Acc_noise", 1.0 * correct_d1 / total, epoch)
         log_metric("Train/Classifier_loss", classifier_loss / (batch_idx + 1), epoch)
         log_metric(
             "Train/Discriminator_loss", discriminator_loss / (batch_idx + 1), epoch
@@ -134,58 +132,59 @@ class Adaptation:
         log_metric("Train/Acc_domain", 1.0 * correct_domain / (2 * total), epoch)
 
     def test(self, epoch, alpha):
-        self.model.eval()
+        self.embedding.eval()
+        self.classifier.eval()
         classifier_loss = 0
         discriminator_loss = 0
         net_loss = 0
-        correct = 0
+        correct_d0 = 0
+        correct_d1 = 0
         correct_domain = 0
-        correct_noisy = 0
         total = 0
         with torch.no_grad():
             for batch_idx, (images, labels) in enumerate(self.testloader):
 
-                images = images.to(self.device)
+                images_d0 = images.to(self.device)
                 labels = labels.to(self.device)
-                images_noisy = images + self.perturb * random(images.shape, self.device)
+                images_d1 = images_d0 + self.perturb * random(images.shape, self.device)
 
-                outputs = self.model(images)
-                outputs_noisy = self.model(images_noisy)
+                embeddings_d0 = self.embedding(images_d0)
+                embeddings_d1 = self.embedding(images_d1)
 
-                classify, original = self.final_layers(outputs, alpha)
-                classify_noisy, noisy = self.final_layers(outputs_noisy, alpha)
+                outputs_d0_class, outputs_d0 = self.classifier(embeddings_d0, alpha)
+                outputs_d1_class, outputs_d1 = self.classifier(embeddings_d1, alpha)
 
+                # 0 for original, 1 for noisy
                 domains = torch.cat(
                     (
                         torch.zeros(images.size(0), device=self.device),
                         torch.ones(images.size(0), device=self.device),
                     )
                 ).long()
-                discriminate = torch.cat((original, noisy))
+                outputs_domains = torch.cat((outputs_d0, outputs_d1))
 
                 loss_class, loss_dis = self.calc_loss(
-                    classify, discriminate, labels, domains
+                    outputs_d0_class, outputs_domains, labels, domains
                 )
                 loss = loss_class + loss_dis
-
                 classifier_loss += loss_class.item()
                 discriminator_loss += loss_dis.item()
                 net_loss += loss.item()
 
-                predicted = classify.argmax(1)
-                correct += predicted.eq(labels).sum().item()
+                predicted_d0 = outputs_d0_class.argmax(1)
+                correct_d0 += predicted_d0.eq(labels).sum().item()
 
-                predicted_noisy = classify_noisy.argmax(1)
-                correct_noisy += predicted_noisy.eq(labels).sum().item()
+                predicted_d1 = outputs_d1_class.argmax(1)
+                correct_d1 += predicted_d1.eq(labels).sum().item()
 
-                predicted_domain = discriminate.argmax(1)
+                predicted_domain = outputs_domains.argmax(1)
                 correct_domain += domains.eq(predicted_domain).sum().item()
 
                 total += labels.size(0)
 
         log_metric("Test/Net_loss", net_loss / (batch_idx + 1), epoch)
-        log_metric("Test/Acc_clean", 1.0 * correct / total, epoch)
-        log_metric("Test/Acc_noise", 1.0 * correct_noisy / total, epoch)
+        log_metric("Test/Acc_clean", 1.0 * correct_d0 / total, epoch)
+        log_metric("Test/Acc_noise", 1.0 * correct_d1 / total, epoch)
         log_metric("Test/Classifier_loss", classifier_loss / (batch_idx + 1), epoch)
         log_metric(
             "Test/Discriminator_loss", discriminator_loss / (batch_idx + 1), epoch
@@ -195,19 +194,22 @@ class Adaptation:
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model", default="resnet56", type=str, help="Model architecture"
+    )
     parser.add_argument("--batch_size", default=128, type=int, help="Batch Size")
     parser.add_argument(
-        "--epochs", default=100, type=int, help="Number of training epochs"
+        "--epochs", default=150, type=int, help="Number of training epochs"
     )
     parser.add_argument(
         "--perturb", type=float, default=10.0, help="Magnitude of noise to the input"
     )
-    parser.add_argument("--lr", default=0.01, type=float, help="learning rate")
+    parser.add_argument("--lr", default=0.1, type=float, help="learning rate")
     parser.add_argument(
-        "--step", default=20, type=int, help="Step size of LR scheduler"
+        "--step", default=30, type=int, help="Step size of LR scheduler"
     )
     parser.add_argument(
-        "--gamma", default=0.2, type=float, help="Gamma of LR scheduler"
+        "--gamma", default=0.1, type=float, help="Gamma of LR scheduler"
     )
 
     args = parser.parse_args()
@@ -216,15 +218,18 @@ def main():
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    classifier = ResNet18().to(device)
-    classifier = nn.DataParallel(classifier)
+    embedding = resnet.__dict__[args.model]()
+    # Remove the last layer
+    embedding.linear = nn.Identity()
+    embedding = embedding.to(device)
+    embedding = nn.DataParallel(embedding)
     # checkpoint = torch.load("ckpt.pth", map_location=device)
-    # classifier.load_state_dict(checkpoint["net"])
-    discriminator = Net().to(device)
-    discriminator = nn.DataParallel(discriminator)
+    # embedding.load_state_dict(checkpoint["net"])
+    classifier = Classify().to(device)
+    classifier = nn.DataParallel(classifier)
     cuda.benchmark = True
 
-    params = list(classifier.parameters()) + list(discriminator.parameters())
+    params = list(embedding.parameters()) + list(classifier.parameters())
     optimizer = Adam(params, lr=args.lr)
     scheduler = lr_scheduler.StepLR(optimizer, step_size=args.step, gamma=args.gamma)
     trainloader, testloader = read_vision_dataset("./data", args.batch_size)
@@ -236,8 +241,8 @@ def main():
 
         criterion = nn.CrossEntropyLoss()
         Obj = Adaptation(
+            embedding,
             classifier,
-            discriminator,
             criterion,
             trainloader,
             testloader,
@@ -256,8 +261,8 @@ def main():
             scheduler.step()
 
         log_artifacts(output_dir, artifact_path="events")
-        mlflow.pytorch.log_model(classifier, artifact_path="model-base")
-        mlflow.pytorch.log_model(discriminator, artifact_path="model-head")
+        mlflow.pytorch.log_model(embedding, artifact_path="model-base")
+        mlflow.pytorch.log_model(classifier, artifact_path="model-head")
 
 
 if __name__ == "__main__":
